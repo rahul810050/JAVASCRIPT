176cb8defcf7277cf6289305d567d9a62aee38dc
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-28 18:29:33 +0530

    learned JS Variables

---
+++ 01_variavbles.js
@@ -0,0 +1,17 @@
+const accountId = 673473 // CHANGING CONSTANT VARIABLES IS NOT ALLOWED
+let Email = "rahul@gmail.com"
+var pass = "rahul2004"
+city = "Kolkata"
+let state;
+
+
+Email = "Cocane@gmail.com"
+pass = "cocane2004"
+city = "Lucknow"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,Email,pass,city,state])
