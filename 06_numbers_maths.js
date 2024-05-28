17c124c55ba6f47176d9be38fef0e8910f9c9252
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-29 02:15:31 +0530

    learned numbers and math functions in JS

---
+++ 06_numbers_maths.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const num1 = 7283.894
+// console.log(num1.toPrecision(4));
+
+// const num2 = 1000000
+// console.log(num2.toLocaleString());
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.parseInt("7863"));
+
+
+
+//****************** Math ********************
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
