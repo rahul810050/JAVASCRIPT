62067da5fb490ec7adb572ad394c415aa95ba62b
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-28 17:10:30 +0530

    learned how if else conditions work

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 console.log("Hello world");
 console.log("just solved all the command misunderstanding");
 console.log("JAVASCRIPT");
+let age = 20;
+if(age<18 && age>=12){
+	console.log("you are a teenager..You cannot vote!!")
+}
+else if(age < 12){
+	console.log("You are in school..")
+}
+else{
+	console.log("Congratulation...You can vote!!");
+	
+}
