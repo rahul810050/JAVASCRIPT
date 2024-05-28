f66fabcfd9a5dbb5468d43f27a563bae0826b6d9
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-29 00:02:23 +0530

    learned how Symbol works

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -10,16 +10,27 @@
 // undefined =>
 // symbol => unique
 
+// const id = Symbol('282')
+// const anotherId = Symbol('282')
+
+// console.log(id === anotherId); // false
+
 
 // object
 
-let age = 20
+// let age = 20
 
-let name = "Rahul"
+// let name = "Rahul"
 
 
-console.log(typeof age);
-console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof name);
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// ****************************SUMMARY OF DATATYPES*************************
+
+// Primitive -> 7 types-> String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive) -> Array, Object, Functions
---
+++ 04_comparision.js
@@ -0,0 +1,9 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// == checks only value
+// === checks value as well as datatype
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
