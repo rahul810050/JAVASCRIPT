719e778e1e03f5a7d5f67b404b83d7b88ae7b54f
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-28 21:55:42 +0530

    learned datatypes in JS

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodeJs not browser
+
+// number => 2 to power 53
+// BigInt
+// striing => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+let age = 20
+
+let name = "Rahul"
+
+
+console.log(typeof age);
+console.log(typeof name);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
