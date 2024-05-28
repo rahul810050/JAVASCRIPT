b4a3dd198df66571b3a8e752dcd2d0d2507d5017
Rahul Naskar <rahul9875458880@gmail.com>
2024-05-28 22:22:40 +0530

    learned convertion operation in JS

---
+++ 03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let age = "20fdhgv"
+
+// console.log(typeof age);
+// console.log(typeof (age));
+
+// let bool = true;
+
+// let boolInNumber = Number(bool);
+
+// console.log(boolInNumber);
+
+// let valueInNumber = Number(age);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let something = undefined
+// let somethingInNumber = Number(something);
+
+// console.log(somethingInNumber);
+
+
+
+// "20" => 20
+// "20khdc" => NaN
+// true => 1; false => 0;
+
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "empty" => false;
+// "rahul" => true
+
+
+let number = 20
+
+let stringNumber = String(number)
+
+console.log(typeof(stringNumber));
+console.log((stringNumber));
