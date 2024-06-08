ffbee95f9a4ce0709f3caefe3dac2ad3cc654ff7
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-08 17:13:55 +0530

    Talk about De-structure & JSON API

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,9 +5,9 @@ tinderUser.id = "xyz1234"
 tinderUser.name = "Rahul"
 tinderUser.age = 20
 
-console.log(Object.keys(tinderUser)); // Returns the names of the enumerable string properties and methods of an object.
-console.log(Object.values(tinderUser)); // Returns an array of values of the enumerable properties of an object
-console.log(Object.entries(tinderUser)); // Returns an array of key/values of the enumerable properties of an object
+// console.log(Object.keys(tinderUser)); // Returns the names of the enumerable string properties and methods of an object.
+// console.log(Object.values(tinderUser)); // Returns an array of values of the enumerable properties of an object
+// console.log(Object.entries(tinderUser)); // Returns an array of key/values of the enumerable properties of an object
 
 // console.log(tinderUser);
 
@@ -28,20 +28,33 @@ const obj2 = {1: "d",2: "e",3: "f"}
 
 // const obj3 = Object.assign({},obj1,obj2)// Assign function Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
 
-const obj3 = {...obj1,...obj2} // we can also use spread operator to merge two or more object
+const obj3 = {...obj1,...obj2} // we can also use spread operator to merge two or more objects
 // console.log(obj3);
 
 
-const obj = [
-	{
-		id : 6465876,
-		email: "Rahul@gmail.com"
-	},
-	{
+// const obj = [
+// 	{
+// 		id : 6465876,
+// 		email: "Rahul@gmail.com"
+// 	},
+// 	{
 
-	},
-	{
+// 	},
+// 	{
 
-	}
-]
+// 	}
+// ]
 
+
+
+const course = {
+	courseName: "JavaScript",
+	price: "10000",
+	courseInstructor: "Rahul"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // it is called destructure of JS
+// console.log(courseInstructor);
+console.log(instructor);
