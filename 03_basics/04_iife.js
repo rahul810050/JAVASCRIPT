bcbd70b8d9c227f0a758b00cf2e66c31b3ac3f26
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-11 01:01:15 +0530

    IIFE is JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function func(){ // named iife
+	console.log(`DB CONNECTED`);
+}) (); // iife function does not know where to end that's why we need to add the semicolon at end of function
+
+// ( () => { // unnamed iife
+// 	console.log("DB CONNECTED Immediately");
+// })();
+
+// why iife fucntion is used??
+// Ans:- iife function is a function whhich get executed immediately to solve the problem of global scope polutions
+
+// (function definition) (function execution)
+
+
+( (name) => { // unnamed iife
+	console.log(`DB CONNECTED Immediately ${name}`);
+})("Rahul");
