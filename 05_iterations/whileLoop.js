c2f00d0b5d9542536488829f83b78285eb5a01ca
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-11 20:46:37 +0530

    while and do while loop

---
+++ 05_iterations/whileLoop.js
@@ -0,0 +1,21 @@
+// let i = 0
+// while(i <= 10){
+// 	console.log(i);
+// 	i += 2
+// }
+
+
+// let myArray = [100,200,300,400,500,600]
+// let i = 0
+// while(i < myArray.length){
+// 	console.log(myArray[i]);
+// 	i++
+// }
+
+// ++++++++ do while +++++++++
+
+let score = 11
+
+do {
+	console.log(score);
+} while (score <= 10);
