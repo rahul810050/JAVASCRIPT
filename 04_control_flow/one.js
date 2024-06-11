e8ccfc2dccf440eb2ded461b8bd074cd7205b0b8
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-11 11:18:38 +0530

    if else is JS

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+const score = 200
+
+if(score > 100){
+	let power = "fly"
+	console.log(`User power: ${power}`);
+}
+
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+	console.log("Allow to buy products");
+}
