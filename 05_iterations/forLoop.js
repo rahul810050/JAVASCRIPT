10cc5e5fd1336e5febda0d1c38941bb14bb5ec8c
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-11 16:20:42 +0530

    for loop in JS

---
+++ 05_iterations/forLoop.js
@@ -0,0 +1,12 @@
+// for loop
+
+for(let i = 0; i < 10; i++){
+	const element = i;
+	//console.log(i);
+}
+
+const myArray = [100,200,300,400,500,600]
+
+for (let i = 0; i < myArray.length; i++) {
+	//console.log(myArray[i]);
+}
