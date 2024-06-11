e52a07ce456876cd2d3309359b2620496354f978
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-11 12:53:49 +0530

    switch case in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+// 	case value:
+		
+// 		break;
+
+// 	default:
+// 		break;
+// }
+
+const day = 6
+switch (day) {
+	case 1:
+		console.log("it's sunday");
+		break;
+	case 2:
+		console.log("it's monday");
+		break;
+	case 3:
+		console.log("it's tuesday");
+		break;
+	case 4:
+		console.log("it's wednesday");
+		break;
+	case 5:
+		console.log("it's thursday");
+		break;
+	case 6:
+		console.log("it's friday");
+		break;
+	case 7:
+		console.log("it's saturday");
+		break;
+
+	default:
+		console.log("it's not a valid day");
+		break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,8 @@
+const userEmail = "rahul@gmail.com"
+
+if(userEmail) console.log("got user email");
+else console.log("dont have any email??");
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
