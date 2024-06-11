4cd4a54ab0573a32412430ac72d380daa8bddf23
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-12 01:19:32 +0530

    for of loop and for in loop in JS

---
+++ 05_iterations/forInLoop.js
@@ -0,0 +1,33 @@
+
+const myObj = {
+	js: "javascript",
+	cpp: "c++",
+	rb: "ruby",
+	swift: "swift by apple"
+}
+
+// for (const key in myObj) { // object is iterable by forin loop
+// 	console.log(`${key} :- ${myObj[key]}`);
+// }
+
+
+// Array
+
+const programming = ["js","rb","py","cpp"]
+
+// for (const key in programming) {
+// 	console.log(key); // it prints the indexes of the array
+// 	console.log(programming[key]); // it prints the value of the array
+// }
+
+// forin in map
+
+const map = new Map()
+map.set(1,"INDIA")
+map.set(2,"RUSSIA")
+map.set(3,"USA")
+map.set(4,"UKRAIN")
+
+for (const key in map) {
+	console.log(key);// does not work 
+}
---
+++ 05_iterations/forOfLoop.js
@@ -0,0 +1,41 @@
+// for of loop 
+
+const a = ["","","",""]
+
+const b = [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const i of arr) {
+	//console.log(i);
+}
+
+// const str = "Rahul Naskar"
+// for (const i of str) {
+// 	if(i === " ") continue
+// 	else console.log(`each char: ${i}`);
+// }
+
+const map = new Map()
+map.set(1,"INDIA")
+map.set(2,"RUSSIA")
+map.set(3,"USA")
+map.set(4,"UKRAIN")
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+// 	console.log(key,":-",value);
+// }
+
+// object
+
+const obj = {
+	name: "rahul",
+	age: 20
+}
+// console.log(obj);
+
+// for (const [key,value] of obj) { // object is not iterable like this forof does not work
+// console.log(key,":-",value);
+// }
