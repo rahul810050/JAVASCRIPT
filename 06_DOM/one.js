78abe23bb529ccff92d1cfcf106b8e705ee37074
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-13 05:31:12 +0530

    getElementById, class, querySelector, basic DOM manipulation

---
+++ 06_DOM/one.js
@@ -0,0 +1,16 @@
+const a = document.getElementsByClassName("list")
+const b = Array.from(a)
+b.forEach((l)=>{
+	console.log(l)
+})
+
+// const a = document.getElementById("title")
+// a.innerHTML-> DOM learning <span style="display: none;">hello world</span>'
+// a.innerText -> 'DOM learning'
+// a.textContent -> 'DOM learning hello world'
+
+
+
+console.log(document.links) 
+console.log(document.links[2])
+document.getElementsById('firstHeading').innerHTML = "<h1>RAHUL</h1>"
