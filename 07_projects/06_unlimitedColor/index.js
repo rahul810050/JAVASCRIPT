62a83783a7b1126d7968c3922ca984459fe8446f
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-20 14:53:59 +0530

    unlimited background color changing

---
+++ 07_projects/06_unlimitedColor/index.js
@@ -0,0 +1,25 @@
+function getRandomColor(){
+  const letter = '0123456789ABCDEF'
+  let color = '#'
+  
+  for(let i = 0; i < 6; i++){
+    const randomColor = letter[Math.floor(Math.random() * 16)]
+    color += randomColor
+  }
+  return color
+
+}
+
+let interval;
+
+document.querySelector('#start').addEventListener('click',function(e){
+  interval = setInterval(function(){
+    const ranndom = getRandomColor()
+    document.querySelector('body').style.backgroundColor = ranndom
+  }, 2000)
+})
+
+
+document.querySelector('#stop').addEventListener('click',function(e){
+  clearInterval(interval)
+})
